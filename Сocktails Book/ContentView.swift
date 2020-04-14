58cbf9c60f188baee55c06a4ace4030037e40511
JustTeRoR <pavlenok2000@gmail.comgit config --global user.email pavlenok2000@gmail.com>
2020-04-14 22:31:26 +0300

    Initial Commit

---
+++ Сocktails Book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Сocktails Book
+//
+//  Created by Сергей Павленок on 4/14/20.
+//  Copyright © 2020 just_terror. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
