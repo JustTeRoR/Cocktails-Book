8df2dc0680dee12ca0a8e7cc53499d2d64b2a046
JustTeRoR <pavlenok2000@gmail.com>
2020-04-17 23:40:10 +0300

    Added start screen and basic controller (MainViewController)

---
+++ Coctails Book/Coctails Book/Controllers/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  Coctails Book
+//
+//  Created by Сергей Павленок on 4/17/20.
+//  Copyright © 2020 just_terror. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
