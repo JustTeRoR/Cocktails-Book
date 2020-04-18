61aef39033e4b1a61945492cd98089b91e087d00
JustTeRoR <pavlenok2000@gmail.com>
2020-04-18 13:07:02 +0300

    Added class constructor

--- Coctails Book/Coctails Book/Models/Cocktail.swift
+++ Coctails Book/Coctails Book/Models/Cocktail.swift
@@ -12,6 +12,20 @@ import Foundation
 class Cocktail
 {
     private var cocktailName_:String = ""
-    private var cocktailRecipe:String = ""
+    private var cocktailCategory_:String = ""
+    private var cocktailAlcohol_:String = ""
+    private var glassForCocktail_:String = ""
+    private var cocktailRecipe_:String = ""
+    private var ingredientList_ = [Ingredient]()
+    
+    public func Cocktail(cocktailName: String, cocktailCategory: String, cocktailAlcohol: String, glassForCocktail: String, cocktailRecipe:String, ingredientList: [Ingredient])
+    {
+        cocktailName_ = cocktailName
+        cocktailCategory_ = cocktailCategory
+        cocktailAlcohol_  = cocktailAlcohol
+        glassForCocktail_ = glassForCocktail
+        cocktailRecipe_ = cocktailRecipe
+        ingredientList_ = ingredientList
+    }
     
 }
